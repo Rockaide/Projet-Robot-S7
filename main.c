ae6dddb835868dc2afd7e3597b5ca157e80965ba
Rockaide <78188200+Rockaide@users.noreply.github.com>
2024-01-22 11:20:20 +0100

    Ajoute des fonctions sonar

--- main.c
+++ main.c
@@ -112,6 +112,10 @@ uint32_t OV = 0;
 int cpt = 1;
 /* USER CODE END PV */
 
+/*Variables sonar*/
+volatile uint32_t dist_sonar = 0;
+volatile uint8_t fin_lect_sonar = 0;
+
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_NVIC_Init(void);
@@ -121,6 +125,7 @@ void regulateur(void);
 void controle(void);
 void Calcul_Vit(void);
 void ACS(void);
+void lecture_sonar(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -176,6 +181,7 @@ int main(void)
     	New_CMDE = 1;
     	HAL_ADC_Start_IT(&hadc1);
     	HAL_TIM_Base_Start_IT(&htim2);  // Start IT sur front montant PWM
+    	HAL_TIM_IC_Start_IT (&htim1, TIM_CHANNEL_2); 	// Start input capture
     	HAL_TIM_Encoder_Start(&htim3, TIM_CHANNEL_ALL);
     	HAL_TIM_Encoder_Start(&htim1, TIM_CHANNEL_ALL);
     	HAL_UART_Receive_IT(&huart3, &BLUE_RX, 1);
@@ -183,7 +189,7 @@ int main(void)
     	HAL_TIM_PWM_Start_IT(&htim1,TIM_CHANNEL_4); //Interruption PWM sonar
     	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_4); //Début PWM sonar
   /* USER CODE END 2 */
-/**/
+
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
@@ -191,7 +197,7 @@ int main(void)
 	  //__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 2000);
 	  Gestion_Commandes();
 	  controle();
-
+	  lecture_sonar();
 
 
 /*	  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 1050); //regard 90° droite
@@ -1143,6 +1149,27 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 	New_CMDE = 1;
 }
 
+void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
+{
+
+	if ( htim->Instance == TIM1 )
+	{
+		//lecture de la valeur
+		dist_sonar = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_2);
+		//raz du gpio du trig
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_RESET);
+		//fin lecture a true
+		fin_lect_sonar = 1;
+	}
+}
+
+void lecture_sonar()
+{
+	//Trigger sonar
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_SET);
+	//Attendre 50ms
+	fin_lect_sonar = 0;
+}
 /* USER CODE END 4 */
 
 /**
